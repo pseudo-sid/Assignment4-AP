04f6f2ba182049091e626917bb5950cb62fc3d88
Siddharth Singh <siddharth19111@iiitd.ac.in>
2020-11-30 12:23:15 +0530

    Node Class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.*;
+
+class Node{
+    private long v;
+    private int deg;
+    private ArrayList<Node> Children;
+    Node(){
+        Random rand = new Random();
+        Children = new ArrayList<>();
+        v = 1 + rand.nextInt(1000000);
+        deg = 0;
+    }
+    void addChild(Node x){
+        Children.add(x);
+        deg++;
+    }
+    public int getDeg() {
+        return deg;
+    }
+}
 public class Main {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input the number of nodes in tree: ");
+        int N = sc.nextInt();
+        //Tree creation code
+
+        //Node check code
 
     }
 }
